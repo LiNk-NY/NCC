d992f9358d8389dadd9152cd90b26f1b1c4dbdf9
LiNk-NY <mramos09@gmail.com>
2017-02-26 00:06:41 -0500

    create data dir if absent

--- R/download.R
+++ R/download.R
@@ -9,9 +9,13 @@ library(dplyr)
 ## Authenticate to Dropbox API at FIRST RUN
 # drop_auth()
 # drop_acc() %>% select(uid, display_name, email_verified, quota_info.quota)
+
 dataFiles <- rdrop2::drop_dir("NCC_PROJECT/DATA")[["path"]]
 savData <- grepl("^[^A].+\\.sav$", basename(dataFiles))
 dataFiles <- dataFiles[savData]
+
+if (!dir.exists("data"))
+    dir.create("data")
 
 invisible(lapply(dataFiles, function(file) {
     drop_get(file, local_file = file.path("data", basename(file)))
