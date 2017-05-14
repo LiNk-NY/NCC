ca96cde22af458c5d5c6885e75ec3904a823fb31
LiNk-NY <mramos09@gmail.com>
2017-05-13 20:40:34 -0400

    rename file

--- R/readCleanMerge.R
+++ R/readCleanMerge.R
@@ -1,17 +1,7 @@
+## Cleaning and merging datasets
+## NOTE. run R/download.R to download data
+
 ## Read NCC SAV datasets
-
-## source("https://bioconductor.org/biocLite.R")
-## BiocInstaller::biocLite("IRanges")
-
-suppressPackageStartupMessages({
-    library(haven)
-    library(readr)
-    library(dplyr)
-    library(IRanges)
-    library(stringdist)
-    library(purrr)
-})
-
 dataList <- list.files("data", pattern = "\\.sav$", full.names = TRUE)
 names(dataList) <- gsub(".sav", "", basename(dataList), fixed = TRUE)
 
@@ -68,12 +58,6 @@ for (i in seq_along(timeVaryingNames)) {
 
 ## Check all vars match across datasets
 stopifnot(!length(Reduce(outersect, timeVaryingNames)))
-
-endings <- IRanges::CharacterList(
-    Baseline = paste(c("YN$", "y$", "Ca$"), collapse = "|"),
-    M12Scan = paste(c("YNY$", "Y$", "YY$", "CY$", "M12$"), collapse = "|"),
-    M1Scan = paste(c("YN1$", "1$", "Y1$", "C1$", "M1$"), collapse = "|"),
-    M6Scan = paste(c("YN6$", "6$", "Y6$", "C6$", "M6$"), collapse = "|"))
 
 newNames <- S4Vectors::mendoapply(function(patterns, varnames) {
     gsub(patterns, "", varnames, ignore.case = FALSE) ## Cases correct
@@ -189,13 +173,14 @@ for (i in seq_along(newDataList)) {
                                                        nrow(newDataList[[i]])),
                                            .after = 1)
 }
-
+## Bind all time points
 NCCdata <- dplyr::bind_rows(newDataList)
 
+## Full dataset with missing observations (time points)
 NCCdata <- dplyr::arrange(NCCdata, ID, MONTH)
 
-## Tally by ID entries
-group_by(NCCdata, ID) %>% summarize(N = n())
+## Tally by ID entries (interactive)
+# group_by(NCCdata, ID) %>% summarize(N = n())
 
 # Impute missing timepoints
 fullNCC <- split(NCCdata, NCCdata[["ID"]]) %>%
@@ -209,77 +194,5 @@ NCCdf <- dplyr::bind_rows(fullNCC)
 charVars <- vapply(NCCdf, is.character, logical(1L))
 NCCdf[, charVars] <- readr::type_convert(NCCdf[, charVars])
 
-## Create codebook data frame for extracting data from variable names
-infoFrame <- data.frame(
-    pattern = c("^2[A-Z]*", "^3[A-Z]*", "^[23]A.", "^[23]B.",
-                "^[23]C.", "^[23]D.", "^[23]E.", "^[23][A-E]1.",
-                "^[23][A-E]2.", "^[23][A-E]3.", "^[23][A-E][1-3]A",
-                "^[23][A-E][1-3]B1", "^[23][A-E][1-3]B2", "^[23][A-E][1-3]C"),
-    interpretation = c("LeftHemi", "RightHemi", "Frontal", "Temporal",
-                       "Parietal", "Occipital", "Basal", "Active",
-                       "Transitional", "Inactive", "Parenchymal",
-                       "SubarachNumber", "SubarachClust", "Indeterminate"),
-    level = c(rep("First", 2), rep("Second", 5), rep("Third", 3),
-              rep ("Fourth", 4)),
-    variableName = c(rep("Hemisphere", 2), rep("Area", 5),
-                     rep("Status", 3), rep("Tissue", 4)),
-    stringsAsFactors = FALSE
-)
-
-## Find variable names for right and left hemispheres
-findHemi <- grep("^[23]", names(NCCdf), value = TRUE)
-
-## Obtain hit matrix for matching patterns in variable names
-namesHitMat <- vapply(infoFrame[["pattern"]], function(x) grepl(x, findHemi),
-                      logical(length(findHemi)))
-rownames(namesHitMat) <- findHemi
-
-## Matches in infoFrame for each variable (see interpretation column)
-decodedVariable <- apply(namesHitMat, 1, function(g) infoFrame[g, ])
-
-## Take interpretation column and create new data from variables
-vars <- t(vapply(decodedVariable, function(x) x[["interpretation"]], character(4L)))
-vars <- as.data.frame(vars, stringsAsFactors = FALSE)
-colnames(vars) <- unique(infoFrame[["variableName"]])
-vars$Code <- rownames(vars)
-vars$Status <- factor(vars$Status,
-                      levels = c("Active", "Transitional", "Inactive"),
-                      ordered = TRUE)
-
-regions <- arrange(vars, Tissue, Status) %>%
-    split(., list(.$Hemisphere, .$Area, .$Tissue)) %>%
-    map(function(x) x$Code)
-
-## Example data chunk by region
-regionID <- lapply(regions, function(reg) split(NCCdf[, reg], NCCdf$ID))
-
-## Checker functions
-## No sum of rows greater than 1
-.sumRowsGT1 <- function(x) {
-    !any(apply(x, 1, function(g) { sum(g) > 1 }), na.rm = TRUE)
-}
-
-## Total matrix sum is not zero
-.totalSumZero <- function(x) {
-    sum(x, na.rm = TRUE) != 0L
-}
-
-## Create checker function for data chunks (region by ID)
-.validCystMatrix <- function(x) {
-    stopifnot(nrow(x) == 4L)
-    all(.sumRowsGT1(x), .totalSumZero(x))
-}
-
-.validCystMatrix(regionID[[1L]][[1]])
-
-## Check how many code chunks per region and ID are valid
-lapply(regionID, function(reg) { sum(vapply(reg, function(x) {.validCystMatrix(x)}, logical(1L)))})
-
-## Breakdown valid chunks by region and ID
-lapply(regionID, function(reg) { vapply(reg, function(x) {.validCystMatrix(x)}, logical(1L))})
-
-## Get IDs that have a valid matrix
-lapply(regionID, function(reg) { names(Filter(isTRUE, vapply(reg, function(x) {.validCystMatrix(x)}, logical(1L)))) } )
-
-## Checking example region and ID
-regionID$RightHemi.Temporal.SubarachNumber$`1109`
+## Serialize and store data
+# readr::write_rds(NCCdf, path = "data/NCCmerged.rds")
