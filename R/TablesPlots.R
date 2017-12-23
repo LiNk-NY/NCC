d1f1b1d5dad53a06fbaa3632beefc7b0b7000061
LiNk-NY <mramos09@gmail.com>
2017-12-23 00:31:06 -0500

    separate data products from analysis script

---
+++ R/TablesPlots.R
@@ -0,0 +1,44 @@
+# Script for generating tables and figures
+
+# load("data/statetable.Rda")
+# load("data/modelRes.Rda")
+
+resultTable1 <- do.call(cbind,
+lapply(c(Albendazole = 1, Parenchymal = 2), function(g)
+    apply(round(modelRes[, , g], 2), 1L, function(x)
+        paste(x[1], paste0("(", paste(x[2:3], collapse = " - "), ")")))
+    )
+)
+
+stateChanges <- gsub("State 1", "Active", rownames(resultTable1), fixed = TRUE) %>%
+    gsub("State 2", "Transitional", ., fixed = TRUE) %>%
+    gsub("State 3", "Calcified", ., fixed = TRUE) %>%
+    gsub("State 4", "Dissolved", ., fixed = TRUE)
+
+rownames(resultTable1) <- stateChanges
+
+countstt <- c(txsit[1, 2], txsit[1, 4], txsit[2, 3], txsit[2, 4], txsit[3, 4])
+"n (%)" <- paste(countstt, paste0("(", round(countstt/sum(txsit) * 100, 1), ")"))
+
+resultTable1 <- cbind(`n (%)`, resultTable1)
+
+# write.csv(resultTable1, "data/resultTable1.csv")
+
+#############################
+# plot the survival curve   #
+#############################
+
+par(mfrow=c(1,2))
+plot(fit0, covariate = list(1), show.legend = FALSE,
+        xlab="Time after trial start (months)", las=1)
+legend(x = 8, y = 1,
+    legend = paste("From", c("Active", "Transitional", "Calcified")),
+    cex = 0.9, lty = seq(4-1), col = rainbow(3), lwd = 1)
+title("Albendazole Group")
+
+plot(fit0, covariate = list(0), show.legend = FALSE,
+        xlab="Time after trial start (months)", las=1)
+legend(x = 8, y = 1,
+    legend = paste("From", c("Active", "Transitional", "Calcified")),
+    cex = 0.9, lty = seq(4-1), col = rainbow(3), lwd = 1)
+title("Placebo Group")
--- R/ncc_msm_analysis.R
+++ R/ncc_msm_analysis.R
@@ -55,6 +55,7 @@ filter(ncc, MONTH == 0L) %>% group_by(ID) %>%
 round(62/117 * 100, 2)
 
 txsit <- statetable.msm(STATUS, IDLOC, data=subset(ncc, STATUS!=99))
+# save(txsit, file = "data/statetable.Rda")
 
 sum(statetable.msm(STATUS,IDLOC, data=subset(ncc, STATUS!=99)))
 (treated <- statetable.msm(STATUS, IDLOC, data=subset(ncc, STATUS!= 99 & drug==1)))
@@ -132,45 +133,3 @@ modelRes <- abind(HR = t(HRresult), HR.ci, along = 2L)
 modelRes <- aperm(modelRes, c(3, 2, 1))
 
 # save(modelRes, file = "data/modelRes.Rda")
-# load("data/modelRes.Rda")
-
-resultTable1 <- do.call(cbind,
-lapply(c(Albendazole = 1, Parenchymal = 2), function(g)
-    apply(round(modelRes[, , g], 2), 1L, function(x)
-        paste(x[1], paste0("(", paste(x[2:3], collapse = " - "), ")")))
-    )
-)
-
-stateChanges <- gsub("State 1", "Active", rownames(resultTable1), fixed = TRUE) %>%
-    gsub("State 2", "Transitional", ., fixed = TRUE) %>%
-    gsub("State 3", "Calcified", ., fixed = TRUE) %>%
-    gsub("State 4", "Dissolved", ., fixed = TRUE)
-
-rownames(resultTable1) <- stateChanges
-
-countstt <- c(txsit[1, 2], txsit[1, 4], txsit[2, 3], txsit[2, 4], txsit[3, 4])
-"n (%)" <- paste(countstt, paste0("(", round(countstt/sum(txsit) * 100, 1), ")"))
-
-resultTable1 <- cbind(`n (%)`, resultTable1)
-
-# write.csv(resultTable1, "data/resultTable1.csv")
-
-#############################
-# plot the survival curve   #
-#############################
-
-par(mfrow=c(1,2))
-plot(fit0, covariate = list(1), show.legend = FALSE,
-        xlab="Time after trial start (months)", las=1)
-legend(x = 8, y = 1,
-    legend = paste("From", c("Active", "Transitional", "Calcified")),
-    cex = 0.9, lty = seq(4-1), col = rainbow(3), lwd = 1)
-title("Albendazole Group")
-
-plot(fit0, covariate = list(0), show.legend = FALSE,
-        xlab="Time after trial start (months)", las=1)
-legend(x = 8, y = 1,
-    legend = paste("From", c("Active", "Transitional", "Calcified")),
-    cex = 0.9, lty = seq(4-1), col = rainbow(3), lwd = 1)
-title("Placebo Group")
-
