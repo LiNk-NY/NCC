c686e91c936c27d3568b241943805006d3ce1d79
LiNk-NY <mramos09@gmail.com>
2018-02-27 23:27:30 -0500

    filter for files, update functions in rdrop2

--- R/download.R
+++ R/download.R
@@ -11,18 +11,21 @@ library(dplyr)
 # drop_acc()
 
 # Not working -- API broken?
-dataFiles <- rdrop2::drop_dir("NCC_PROJECT/DATA")[["path"]]
-savData <- grepl("^[^A].+\\.sav$", basename(dataFiles))
-dataFiles <- dataFiles[savData]
+dataFiles <- rdrop2::drop_dir("NCC_PROJECT/DATA")[["path_display"]]
+savFiles <- grep("\\.sav$", basename(dataFiles), value = TRUE, ignore.case = TRUE) %>%
+    grep("Medical|NCC", ., value = TRUE, invert = TRUE)
+dataFiles <- dataFiles[basename(dataFiles) %in% savFiles]
 
 if (!dir.exists("data"))
     dir.create("data")
 
 invisible(lapply(dataFiles, function(file) {
-    drop_get(file, local_file = file.path("data", basename(file)))
+    drop_download(file, local_file = file.path("data", basename(file)))
 })
 )
 
 ## Rename files
 file.rename("data/Baseline SCan & Drug.sav", "data/BaselineScanDrug.sav")
 file.rename("data/24 Month Scan SPSS-updated-9.24.17.sav", "data/M24Scan.sav")
+file.rename("data/Adult & Pediatric Patient Information Form August162004.sav",
+            "data/patinfo.sav")
